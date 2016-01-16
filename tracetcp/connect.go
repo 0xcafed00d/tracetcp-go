b1a4dd3c60c61720596577e332fd27127bab1a31
Lee Witek <neomech@gmail.com>
2016-01-16 22:03:40 +0000

    setting up

--- tracetcp/connect.go
+++ tracetcp/connect.go
@@ -1,6 +1,64 @@
 package tracetcp
 
 import (
-	"net"
+	"fmt"
 	"syscall"
+	"time"
 )
+
+func connect(host string, port, ttl int, timeout time.Duration) error {
+	sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_TCP)
+	if err != nil {
+		return err
+	}
+	defer syscall.Close(sock)
+
+	err = syscall.SetsockoptInt(sock, 0x0, syscall.IP_TTL, ttl)
+	if err != nil {
+		return err
+	}
+
+	err = syscall.SetNonblock(sock, true)
+	if err != nil {
+		return err
+	}
+
+	addr, err := LookupAddress(host)
+	if err != nil {
+		return nil
+	}
+
+	// ignore error from connect in non-blocking mode. as it will always return a
+	// in progress error
+	_ = syscall.Connect(sock, &syscall.SockaddrInet4{Port: 80, Addr: addr})
+
+	name, err := syscall.Getsockname(sock)
+	fmt.Println(err, name)
+
+	fdset := &syscall.FdSet{}
+	timeoutVal := &syscall.Timeval{}
+	timeoutVal.Sec = int64(timeout / time.Second)
+	timeoutVal.Usec = int64(timeout-time.Duration(timeoutVal.Sec)*time.Second) / 1000
+
+	fmt.Println(timeoutVal)
+
+	FD_ZERO(fdset)
+	FD_SET(fdset, sock)
+
+	start := time.Now()
+	x, err := syscall.Select(sock+1, nil, fdset, nil, timeoutVal)
+	elapsed := time.Since(start)
+
+	fmt.Println(x, elapsed)
+	if err != nil {
+		return err
+	}
+
+	if FD_ISSET(fdset, sock) {
+		fmt.Println("conencted")
+	} else {
+		fmt.Println("timedout")
+	}
+
+	return nil
+}
--- tracetcp/utils.go
+++ tracetcp/utils.go
@@ -3,4 +3,45 @@ package tracetcp
 import (
 	"net"
 	"syscall"
+	"time"
 )
+
+func MakeTimeval(t time.Duration) syscall.Timeval {
+	tv := syscall.Timeval{}
+
+	tv.Sec = int64(t / time.Second)
+	tv.Usec = int64(t-time.Duration(tv.Sec)*time.Second) / 1000
+
+	return tv
+}
+
+func FD_SET(p *syscall.FdSet, i int) {
+	p.Bits[i/64] |= 1 << uint(i) % 64
+}
+
+func FD_ISSET(p *syscall.FdSet, i int) bool {
+	return (p.Bits[i/64] & (1 << uint(i) % 64)) != 0
+}
+
+func FD_ZERO(p *syscall.FdSet) {
+	for i := range p.Bits {
+		p.Bits[i] = 0
+	}
+}
+
+func LookupAddress(host string) ([4]byte, error) {
+	var addr [4]byte
+
+	addresses, err := net.LookupHost(host)
+	if err != nil {
+		return addr, err
+	}
+
+	ip, err := net.ResolveIPAddr("ip", addresses[0])
+	if err != nil {
+		return addr, err
+	}
+
+	copy(addr[:], ip.IP.To4())
+	return addr, nil
+}
