2468b86fe23886d272d17c44e1b278898d65218e
Lee Witek <neomech@gmail.com>
2016-02-21 23:35:55 +0000

    parsing and validating query params

--- cmd/tracetcpserver/tracetcpserve.go
+++ cmd/tracetcpserver/tracetcpserve.go
@@ -5,7 +5,9 @@ import (
 	"io"
 	"net/http"
 	"os/exec"
+	"strconv"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -36,19 +38,37 @@ func validate(s string) bool {
 	return true
 }
 
-func doTrace(w http.ResponseWriter, host, port string) {
+type traceConfig struct {
+	host     string
+	port     string
+	starthop int
+	endhop   int
+	timeout  time.Duration
+	queries  int
+}
+
+var defaultConfig = traceConfig{
+	host:     "",
+	port:     "http",
+	starthop: 1,
+	endhop:   30,
+	timeout:  1 * time.Second,
+	queries:  3,
+}
+
+func doTrace(w http.ResponseWriter, config traceConfig) {
 	fw := flushWriter{w: w}
 	if f, ok := w.(http.Flusher); ok {
 		fw.f = f
 	}
 
-	if !validate(host) {
+	if !validate(config.host) {
 		fmt.Fprint(w, "Invalid Host Name")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	if !validate(port) {
+	if !validate(config.port) {
 		fmt.Fprint(w, "Invalid Port Number")
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -58,7 +78,7 @@ func doTrace(w http.ResponseWriter, host, port string) {
 	cmd.Stdout = &fw
 	cmd.Stderr = &fw
 
-	cmd.Args = append(cmd.Args, host+":"+port)
+	cmd.Args = append(cmd.Args, config.host+":"+config.port)
 
 	err := cmd.Run()
 	if err != nil {
@@ -68,24 +88,74 @@ func doTrace(w http.ResponseWriter, host, port string) {
 }
 
 func doTraceHandler(w http.ResponseWriter, r *http.Request) {
+	config := defaultConfig
 
+	if v, ok := r.URL.Query()["host"]; ok {
+		config.host = v[0]
+	}
+
+	if v, ok := r.URL.Query()["port"]; ok {
+		config.port = v[0]
+	}
+
+	var err error
+
+	if v, ok := r.URL.Query()["starthop"]; ok {
+		config.starthop, err = strconv.Atoi(v[0])
+		if err != nil {
+			fmt.Fprint(w, "Invalid Start Hop: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	if v, ok := r.URL.Query()["endhop"]; ok {
+		config.endhop, err = strconv.Atoi(v[0])
+		if err != nil {
+			fmt.Fprint(w, "Invalid End Hop: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	if v, ok := r.URL.Query()["timeout"]; ok {
+		config.timeout, err = time.ParseDuration(v[0])
+		if err != nil {
+			fmt.Fprint(w, "Invalid Timeout Duration: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	if v, ok := r.URL.Query()["queries"]; ok {
+		config.queries, err = strconv.Atoi(v[0])
+		if err != nil {
+			fmt.Fprint(w, "Invalid Query Count: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	doTrace(w, config)
 }
 
 func execHandler(w http.ResponseWriter, r *http.Request) {
 
-	host := r.FormValue("host")
-	port := r.FormValue("port")
+	config := defaultConfig
+
+	config.host = r.FormValue("host")
+	config.port = r.FormValue("port")
 
 	if r.FormValue("source") == "ok" {
-		host = r.RemoteAddr[:strings.Index(r.RemoteAddr, ":")]
+		config.host = r.RemoteAddr[:strings.Index(r.RemoteAddr, ":")]
 	}
 
-	doTrace(w, host, port)
+	doTrace(w, config)
 }
 
 func main() {
 	http.HandleFunc("/editcmd/", editCommandHandler)
 	http.HandleFunc("/exec/", execHandler)
-	http.HandleFunc("/dotrace/", dotraceHandler)
+	http.HandleFunc("/dotrace", doTraceHandler)
 	http.ListenAndServe(":8080", nil)
 }
